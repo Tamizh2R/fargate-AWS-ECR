f47ea2df053385d97ab17a2c18f1125166ba9937
root <root@ip-172-31-0-202.ap-southeast-2.compute.internal>
2025-04-16 10:04:11 +0000

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('FARGATE IS WORKING WITH JENKINS PIPELINE 2nd test works !!!!!!!!!!\n');
+    res.end('final with testing FARGATE IS WORKING WITH JENKINS PIPELINE 2nd test works !!!!!!!!!!\n');
 });
 
 server.listen(3000, '0.0.0.0', () => {
