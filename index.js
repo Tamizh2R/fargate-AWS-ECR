797b897782d8284d4bd78ca3e9afb3675db061a8
root <root@ip-172-31-0-202.ap-southeast-2.compute.internal>
2025-04-16 09:35:54 +0000

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from AWS Fargate working version-1 is fine !!!!!!!!!!\n');
+    res.end('FARGATE IS WORKING WITH JENKINS PIPELINE!!!!!!!!!!\n');
 });
 
 server.listen(3000, '0.0.0.0', () => {
